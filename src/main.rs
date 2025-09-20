2d018419dccf1d0d5f35b8064d5c1af84102977b
leonardohenriquedc <leonardodamascenodev@gmail.com>
2025-09-19 22:18:23 -0400

    add environments variables

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
-
 use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use std::env;
 
 mod controller;
 mod service;
@@ -8,9 +8,10 @@ mod structs;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     println!("Executando em: {}", env::current_dir().unwrap().display());
     HttpServer::new(|| App::new().configure(controller::config))
-        .bind(("127.0.0.1", 7878))?
+        .bind(("0.0.0.0", 7878))?
         .run()
         .await
 }
--- src/service/get_blogs.rs
+++ src/service/get_blogs.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::fs::{self};
 
 use crate::structs::{
@@ -8,8 +9,17 @@ use actix_web::ResponseError;
 use askama::Template;
 use pulldown_cmark::{Options, Parser, html};
 
-const PATH_FILES_BLOGS: &str = "content/";
-const LINK_DEBUG: &str = "http://localhost:7878/";
+fn return_envs_links(type_number: i8) -> String {
+    let path_files_blogs =
+        env::var("PATH_FILES_BLOGS").expect("não foi possivel ler variavel de ambiente");
+    let link_debug =
+        env::var("LINK_REQUEST_DEBUG").expect("Não foi possivel ler variaveis de ambiente");
+
+    match type_number {
+        1 => path_files_blogs,
+        _ => link_debug,
+    }
+}
 
 pub async fn get_links_of_all_blogs() -> String {
     let mut template_index = TemplateIndex::new();
@@ -17,7 +27,7 @@ pub async fn get_links_of_all_blogs() -> String {
     let name_blogs = get_name_files().await;
 
     for name in name_blogs {
-        let link = format!("{}post/{}", LINK_DEBUG, name);
+        let link = format!("{}post/{}", return_envs_links(2), name);
         let name_format = name.strip_suffix(".md").unwrap().to_string();
         template_index.insert_tag_ancora(link, name_format);
     }
@@ -26,7 +36,7 @@ pub async fn get_links_of_all_blogs() -> String {
 }
 
 pub async fn get_blog_post(name: String) -> String {
-    let path_article = format!("{}{}", PATH_FILES_BLOGS, name);
+    let path_article = format!("{}{}", return_envs_links(1), name);
 
     let article_content =
         fs::read_to_string(path_article.as_str()).expect("Não foi possivel ler arquivo");
@@ -37,7 +47,7 @@ pub async fn get_blog_post(name: String) -> String {
 
     template_view.title = name
         .strip_suffix(".md")
-        .expect("Erro ao remover sufixo")
+        .expect("Erroe ao remover sufixo")
         .to_string();
     template_view.content = content;
 
@@ -63,7 +73,7 @@ fn parsing_md_to_html(content: &str) -> String {
 async fn get_name_files() -> Vec<String> {
     let mut name_blogs: Vec<String> = Vec::new();
 
-    let read_dir = fs::read_dir(PATH_FILES_BLOGS).expect("Não foi possivel localizar pasta");
+    let read_dir = fs::read_dir(return_envs_links(1)).expect("Não foi possivel localizar pasta");
 
     for entry in read_dir {
         match entry {
