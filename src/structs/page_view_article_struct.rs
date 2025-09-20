08eb87a472f5e486a7336e649dad4f408a103ca8
leonardohenriquedc <leonardodamascenodev@gmail.com>
2025-09-19 22:33:29 -0400

    fixing page_view

--- src/structs/page_view_article_struct.rs
+++ src/structs/page_view_article_struct.rs
@@ -1,8 +1,11 @@
+use std::env;
+
 use askama::Template;
 
 #[derive(Template)]
 #[template(path = "layout/page_view_article.html")]
 pub struct TemplatePageViewArticle {
+    link_home: String,
     pub style_sheet: String,
     pub title: String,
     pub content: String,
@@ -10,6 +13,7 @@ pub struct TemplatePageViewArticle {
 impl TemplatePageViewArticle {
     pub fn new() -> Self {
         TemplatePageViewArticle {
+            link_home: env::var("LINK_REQUEST_RELEASE").expect("Não foi possivel ler variavel"),
             style_sheet: String::new(),
             title: String::new(),
             content: String::new(),
